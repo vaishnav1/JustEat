18678058c254da93a70f7d6dc21cea0134f54bfa
VaishnavChidambar <vaishnav.chidambar@sequoia.com>
2022-04-15 13:20:19 +0530

    Initial Commit

---
+++ Takeaway iOS Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Takeaway iOS Assignment
+//
+//  Created by vaishnav.chidambar on 15/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
